27eb1d78a235cabeda6cf2bd3de81e5e62723562
Sonali Prajapati <85176870+sonali251997@users.noreply.github.com>
2022-05-16 16:24:02 +0530

    add users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AddUser from './Components/User/AddUser';
+import UserList from './Components/User/UserList';
 
 function App() {
+
+  const [userslist, setUsersList]= useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return[...prevUserList, {name:uName , age: uAge ,id: Math.random().toString()}];
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <AddUser onAddUser ={addUserHandler}/>
+      <UserList users= {userslist}/>
     </div>
   );
 }
